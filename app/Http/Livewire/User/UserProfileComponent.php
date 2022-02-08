7fa137dd9772574ab042f74016c7fb39d6386c7f
MohamedMonia <ana.m.m2845@gmail.com>
2022-02-08 11:51:34 +0200

    first commit

---
+++ app/Http/Livewire/User/UserProfileComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserProfileComponent extends Component
+{
+    public function render()
+    {
+        $user = User::find(Auth::user()->id);
+        return view('livewire.user.user-profile-component', ['user' => $user])->layout('layouts.base');
+    }
+}
---
+++ resources/views/livewire/user/user-profile-component.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <div class="container" style="padding: 30px 0">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Profile
+                </div>
+                <div class="panel-body">
+                    <div class="col-md-4">
+                        @if ($user->profile->image)
+                            <img src="{{asset('asset/images/profile'}}/{{}}">
+                        @endif
+                    </div>
+                    <div class="col-md-8">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
